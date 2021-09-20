80e2b4a701e6f694340f70e039254627dfd6ff98
Eric Lu <eric.andrew.lu@gmail.com>
2021-09-20 13:42:41 -0700

    coco_device update with singleton initializer

--- src/coco_device.py
+++ src/coco_device.py
@@ -3,7 +3,7 @@ import device_callback as dc
 import time
 
 #Initializes the cocoDevice
-class CocoDeviceInitializer:
+class CocoDevice:
     instance = None
     def __init__(self, cwd, config_path, version, temp_path):
         #Under the hood FFI layer
@@ -13,7 +13,8 @@ class CocoDeviceInitializer:
         self.temp_path = ffi.new("char[]", "{}".format(temp_path).encode('ascii'))
 
     def device_init(self):
-        if CocoDeviceInitializer.instance == None:
+        if CocoDevice.instance == None:
+            print("New Initialization\n")
             device_init_params = ffi.new("coco_device_init_params_t *")
             device_init_params.cwdPath = self.cwd_path
             device_init_params.configFilePath = self.config_path
@@ -26,6 +27,7 @@ class CocoDeviceInitializer:
             device_init_params.tempPath = self.temp_path
 
             ret_val = lib.coco_device_init(device_init_params)
+            print(ret_val)
             if (-1 == ret_val):
                 print("App: coco_device_init failed\n")
                 exit(1)
@@ -33,32 +35,28 @@ class CocoDeviceInitializer:
                 while(-1 == lib.coco_device_init_auth()):
                     print("app: will try re-auth in 3 seconds")
                     time.sleep(3)
-            CocoDeviceInitializer.instance = CocoDeviceInitializer(self.cwd_path, self.config_path, self.firmware_version, self.temp_path)
+            CocoDevice.instance = CocoDevice(self.cwd_path, self.config_path, self.firmware_version, self.temp_path)
         else:
             print("Device is already initialized")
-            return CocoDeviceInitializer.instance
-
-#Ensures cocodevice is a singleton
-class Singleton(CocoDeviceInitializer):
-    _instance = None
-    def __new__(class_, *args, **kwargs):
-        if not isinstance(class_._instance, class_):
-            class_._instance = CocoDevice.__new__(class_, *args, **kwargs)
-        return class_._instance
-
-class CocoDevice(Singleton, CocoDeviceInitializer):
-    pass
+            return CocoDevice.instance
 
 #Main testing
 if __name__ == "__main__":
     callbacks = [dc.coco_device_join_nw_status_cb, dc.coco_device_add_res_status_cb, dc.coco_device_attribute_update_status, dc.coco_device_data_corruption_cb, dc.coco_device_resource_cmd_cb]
-    my_device = CocoDeviceInitializer("/mnt/host/tmp/workspace/cocosdk/cocodevicesdk/examples/c/device-app-boilerplate/build_cwd/is-2",
+    my_device = CocoDevice("/mnt/host/tmp/workspace/cocosdk/cocodevicesdk/examples/c/device-app-boilerplate/build_cwd/is-2",
                                     "/mnt/host/tmp/workspace/cocosdk/cocodevicesdk/examples/c/device-app-boilerplate/build_cwd/is-2/configpython.txt",
                                      "1.0.0",
                                     "/tmp")
-    another_device = CocoDeviceInitializer("/mnt/host/tmp/workspace/cocosdk/cocodevicesdk/examples/c/device-app-boilerplate/build_cwd/is-2",
-                                    "/mnt/host/tmp/workspace/cocosdk/cocodevicesdk/examples/c/device-app-boilerplate/build_cwd/is-2/configpython.txt",
-                                     "1.0.0",
-                                    "/tmp")
-    print(my_device.temp_path == another_device.temp_path)
     my_device.device_init()
+    another_device = CocoDevice("/mnt/host/tmp/workspace/cocosdk/cocodevicesdk/examples/c/device-app-boilerplate/build_cwd/is-2",
+                                    "/mnt/host/tmp/workspace/cocosdk/cocodevicesdk/examples/c/device-app-boilerplate/build_cwd/is-2/configpython.txt",
+                                     "1.0.0",
+                                    "/tmp")
+    another_device.device_init()
+    print("\n\n\n\n")
+    print(my_device.instance == another_device.instance)
+    if my_device.instance != another_device.instance:
+        exit(1)
+    print("\n\n\n\n")
+    while 1:
+        time.sleep(5)
